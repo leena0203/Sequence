eb28fcbebb081250b6a3149a9b646eab75787c79
leena0203 <leenasarode02@gmail.com>
2020-11-11 21:43:10 +0530

    Added: Javascript code to unit conversion

---
+++ unitConversion.js
@@ -0,0 +1,8 @@
+let footInInches = 12; 
+console.log("42 in = " + (42 / footInInches));
+let length = 60, breadth = 40; 
+let footInMeters = 0.3048;
+length *= footInMeters;
+breadth *= footInMeters;
+let numOfPlots = 25;
+console.log("Area of 25 plots = " + (length * breadth * numOfPlots) + " sq m");
