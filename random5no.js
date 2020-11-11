131b44a40a3f805c3e086f3422a43dbb6e362b62
leena0203 <leenasarode02@gmail.com>
2020-11-11 21:26:16 +0530

    Added: Javascript code to find number and average of 5 nos.

---
+++ random5no.js
@@ -0,0 +1,8 @@
+let num1 = Math.floor(Math.random() * 90) + 10;
+let num2 = Math.floor(Math.random() * 90) + 10;
+let num3 = Math.floor(Math.random() * 90) + 10;
+let num4 = Math.floor(Math.random() * 90) + 10;
+let num5 = Math.floor(Math.random() * 90) + 10;
+let sum = num1 + num2 + num3 + num4 + num5
+let average = sum / 5;
+console.log("Sum of 5 no is: "+sum+" And Average is: "+average);
